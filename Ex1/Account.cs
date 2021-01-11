c564859d2f44c5c48c5a2412e6b009734efb0aac
nathanmalone99 <61420616+nathanmalone99@users.noreply.github.com>
2021-01-11 09:46:55 +0000

    Initial setup, made the interface and the Account class

---
+++ Ex1/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    public abstract class Account
+    {
+
+        //properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double balance { get; set; }
+        public int InterestDate { get; set; }
+
+
+        //abstarct method
+        public abstract double CalculateInterset();
+        
+
+
+    }
+}
